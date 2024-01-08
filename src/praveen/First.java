8a99a132620a292907044fa1bf9d637392b6a522
praveen <praveen@gmail.com>
2024-01-08 16:30:30 +0530

    I committing inEXCELR branch

--- src/praveen/First.java
+++ src/praveen/First.java
@@ -3,7 +3,7 @@ package praveen;
 public class First {
 
 	public static void main(String[] args) {
-		System.out.println("Pushing into  Github");
+		System.out.println("Pushing into  Github  Repository");
 
 	}
 
